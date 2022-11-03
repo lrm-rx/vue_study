e7eae813ddb745cc83c762ae6a1bd03ef5068a0e
lrm <756131502@qq.com>
2022-11-03 18:46:57 +0800

    css模块化

---
+++ vite/vite-test/compA.js
@@ -0,0 +1,6 @@
+import compACss from './compA.module.css'
+import compALess from './index.module.less'
+// console.log('compACss', compACss);
+const div = document.createElement("div")
+document.body.appendChild(div)
+div.className=compACss.footer
--- vite/vite-test/main.js
+++ vite/vite-test/main.js
@@ -2,10 +2,13 @@
  * @Author: lrm
  * @Date: 2022-11-03 00:15:25
  * @LastEditors: lrm
- * @LastEditTime: 2022-11-03 15:37:12
+ * @LastEditTime: 2022-11-03 18:24:02
  * @FilePath: \vite\vite-test\main.js
  */
 import { count } from "./counter";
 import "./request"
+import "./index.css"
+import "./compA.js"
+import "./compB.js"
 
 // console.log(count);
