f968ec9353405c08ee197f0afc962a99c4facdcf
lrm <756131502@qq.com>
2023-02-18 17:06:40 +0800

    一些配置

---
+++ my-components/command/build.js
@@ -0,0 +1,104 @@
+const {defineConfig, build} = require('vite')
+const vue = require('@vitejs/plugin-vue')
+const vueJsx = require('@vitejs/plugin-vue-jsx')
+const path = require('path')
+const fsExtra = require('fs-extra')
+const fs = require('fs')
+
+// 打包入口文件夹
+const entryDir = path.resolve(__dirname, '../packages')
+
+// 出口
+const outDir = path.resolve(__dirname, '../lib')
+
+// vite 基础配置
+const baseConfig = defineConfig({
+  configFile: false,
+  publicDir: false,
+  plugins: [vue(), vueJsx()]
+})
+
+// rollup配置
+const rollupOptions = {
+  external: ['vue', 'vue-router'],
+  output: {
+    globals: {
+      vue: 'Vue'
+    }
+  }
+}
+
+// 全量打包构建
+const buildAll = async () => {
+  await build({
+    ...baseConfig,
+    build: {
+      rollupOptions,
+      lib: {
+        entry: path.resolve(entryDir, 'index.ts'),
+        name: 'rm-element-component',
+        fileName: 'rm-elemenu-component',
+        formats: ['es', 'umd']
+      },
+      outDir
+    }
+  })
+}
+
+// 单组件打包构建
+/**
+ * @param {*} name  组件名称
+ */
+const buildSingle = async (name) => {
+  await build({
+    ...baseConfig,
+    build: {
+      rollupOptions,
+      lib: {
+        entry: path.resolve(entryDir, name),
+        name: 'index',
+        fileName: 'index',
+        formats: ['es', 'umd']
+      },
+      outDir: path.resolve(outDir, name)
+    }
+  })
+}
+
+// 每个组件生成pagkage.jsono
+const ceratePackageJson = (name) => {
+  const fileStr = `
+    {
+      "name": "${name}",
+      "main": "index.umd.js",
+      "module": "index.es.js",
+      "style": "styles.css"
+    }
+  `
+  // 输出
+  fsExtra.outputFile(
+    path.resolve(outDir, `${name}/package.json`),
+    fileStr,
+    'uft-8'
+  )
+}
+
+// 打包成库
+const buildLib = async () => {
+  await buildAll()
+
+  // 获取组件名称组成的数组
+  const components = fs.readdirSync(entryDir).filter(name => {
+    const componentDir = path.resolve(entryDir, name)
+    const isDir = fs.lstatSync(componentDir).isDirectory()
+    return isDir && fs.readdirSync(componentDir).includes('index.ts')
+  })
+
+  // 循环构建组件
+  for(const name of components) {
+    await buildSingle(name)
+    ceratePackageJson(name)
+  }
+}
+
+buildLib()
