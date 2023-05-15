b23d5757003ee25d4aff1e6cd783c92e2bcec2ad
lrm <756131502@qq.com>
2023-05-15 23:29:34 +0800

    多组件生命周期执行顺序

---
+++ vue-life-cycle/vue2/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <h1>父组件</h1>
+    <h2>计数: {{ count }}</h2>
+    <button @click="addOne">加1</button>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addOne() {
+      this.count += 1;
+    },
+  },
+  beforeCreate() {
+    console.log("父-beforeCreate");
+  },
+  created() {
+    console.log("父-created");
+  },
+  beforeMount() {
+    console.log("父-beforeMount");
+  },
+  mounted() {
+    console.log("父-mounted");
+  },
+  beforeUpdate() {
+    console.log("父-beforeUpdate");
+  },
+  updated() {
+    console.log("父-updated");
+  },
+  beforeDestroy() {
+    console.log("父-beforeDestroy");
+  },
+  destroyed() {
+    console.log("父-destroyed");
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-life-cycle/vue2/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>我是子组件- {{ count }}</h2>
+    <button @click="increment">加2</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 2,
+    };
+  },
+  methods: {
+    increment() {
+      this.count += 2;
+    },
+  },
+  beforeCreate() {
+    console.log("子-beforeCreate");
+  },
+  created() {
+    console.log("子-created");
+  },
+  beforeMount() {
+    console.log("子-beforeMount");
+  },
+  mounted() {
+    console.log("子-mounted");
+  },
+  beforeUpdate() {
+    console.log("子-beforeUpdate");
+  },
+  updated() {
+    console.log("子-updated");
+  },
+  beforeDestroy() {
+    console.log("子-beforeDestroy");
+  },
+  destroyed() {
+    console.log("子-destroyed");
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
