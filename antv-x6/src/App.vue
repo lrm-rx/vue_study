7c8c647e5322bca619f9d0280fcd3720fb041b18
xiaoming <756131502@qq.com>
2023-07-04 20:20:53 +0800

    antv/x6

---
+++ antv-x6/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  outline: none;
+}
+
+li {
+  list-style-type: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+body {
+  margin: auto;
+  min-width: 1200px;
+  background: #f4f6f9;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.ralative {
+  position: relative;
+}
+
+.fixed {
+  position: absolute;
+}
+
+.fixed {
+  position: fixed;
+}</style>
---
+++ antv-x6/src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  routes: [
+    {
+      path: "/",
+      name: "antv",
+      component: () => import("@/views/antv"),
+    },
+  ],
+});
