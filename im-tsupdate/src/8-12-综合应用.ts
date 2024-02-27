a10d827c227490768cd3f855bfb0fb4afea15e3d
lrm <756131502@qq.com>
2024-02-27 22:50:37 +0800

    ts综合应用

--- im-tsupdate/src/8-12-综合应用.ts
+++ im-tsupdate/src/8-12-综合应用.ts
@@ -7,4 +7,26 @@ interface Todo {
   upt(): number
 }
 
+// 1. 获取Todo函数
+// type Degree<T> = {
+//   [P in keyof T as T[P] extends Function?P:never]:T[P]
+// }
+
+// type DegreeTodo = Degree<Todo>
+
+// 2. 
+type Exc<T> = Exclude<T, Array<any>>
+type Degree<T extends Record<string, any>> = { // 重映射
+  [P in keyof Exc<T> as Exc<T>[P] extends Function?`do${Capitalize<P & string>}`:never]:Exc<T>[P]
+}
+
+type DegreeTodo = Degree<Todo>
+
+
+
+
+type Convert<T> = T extends any ? T: never
+type TestArrayKeys = Convert<keyof Array<any>>
+
+
 export { } 
--- vue3-comp/m-element/vite.config.ts
+++ vue3-comp/m-element/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+// @ts-ignore
 import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
