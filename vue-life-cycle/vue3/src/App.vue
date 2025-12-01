6451f0b2969252f115c69ab0a5b71083479b4f42
ming <756131502@qq.com>
2025-12-01 19:39:45 +0800

    测试封装CryptoUtils方法

--- vue-life-cycle/vue3/src/App.vue
+++ vue-life-cycle/vue3/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>12313</div>
+  <div v-html="testCases"></div>
 </template>
 
 <script lang="ts" setup>
 import { encrypt, decrypt } from "./utils/cryptoUtils";
-const testCases = "<html><body>HTML内容</body></html>";
+const testCases = `<html><body>HTML内容<br/><img src="https://wx3.sinaimg.cn/mw690/d0f8f22agy1hxly5u9tl2j20u0140djg.jpg" /></body></html>`;
 const testCasesEncrypted = encrypt(testCases, "mySecretKey");
 console.log("加密后的内容:", testCasesEncrypted);
 const decryptedContent = decrypt(testCasesEncrypted, "mySecretKey");
